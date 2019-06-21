24b97a9a91c3529fdf9e98076e63646e71e5aca2
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-06-22 11:28:06 +1200

    Update comment.py

--- comment.py
+++ comment.py
@@ -12,38 +12,50 @@ chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(
     ChromeDriverManager().install(), chrome_options=chrome_options)
 
-url = 'https://central.xero.com/s/question/0D51N00004XQXg5SAH/xero-support'
-driver.get(url)
-sleep(3) 
+# open file with URLs
+f=open("urls.txt", "r")
+web_urls = f.read().splitlines()
 
-# go to laurens posts
-authors = driver.find_elements_by_css_selector("span[data-id='005o0000002mTivAAE']")
+session_id = driver.session_id
+count = 0
+# go to each url and find Laurens posts
+for url in web_urls:
+    # maintain the same session
+    driver.session_id = session_id
+    driver.get(url)
+    sleep(3) 
 
-if authors:
-    count = 0
-    # go to each post
-    for author in authors:
-        if 'Lauren' in author.text:
-            # scroll into view
-            sleep(1)
-            author.location_once_scrolled_into_view
+    # go to laurens posts
+    authors = driver.find_elements_by_css_selector("span[data-id='005o0000002mTivAAE']")
 
-            # expand post if it exists
-            try:
-                link = driver.find_element_by_link_text('Expand Post')
-                link.click()
-                image_name = 'image_' + str(count) + '.png'
-                driver.save_screenshot(image_name)
-                count += 1
-                sendSlack(image_name, url)
-            except :
-                image_name = 'image_' + str(count) + '.png'
-                driver.save_screenshot(image_name)
-                count += 1
-                sendSlack(image_name, url)
-        else: pass
-    driver.close()        
+    if authors:
         
-else:
-    driver.close()
+        # go to each post
+        for author in authors:
+            if 'Lauren' in author.text:
+                # scroll into view
+                sleep(1)
+                author.location_once_scrolled_into_view
+
+                # expand post if it exists
+                try:
+                    link = driver.find_element_by_link_text('Expand Post')
+                    link.click()
+                    image_name = 'image_' + str(count) + '.png'
+                    driver.save_screenshot(image_name)
+                    count += 1
+                    sendSlack(image_name, url)
+                except :
+                    image_name = 'image_' + str(count) + '.png'
+                    driver.save_screenshot(image_name)
+                    count += 1
+                    sendSlack(image_name, url)
+            else: pass
+                
+            
+    else:
+        print('No author found')
+driver.close()
+
+print('Done')
 
